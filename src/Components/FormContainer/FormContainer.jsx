2d77baca75b2b8bf74f9f3c67f3e6ec9a44c1616
Daniel Rivera <dayorito@gmail.com>
2023-11-01 17:55:51 -0500

    Se agrego el scrollbar a los formularios

--- src/Components/FormContainer/FormContainer.jsx
+++ src/Components/FormContainer/FormContainer.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import './FormContainer.css'
 
 function FormContainer({name, children}) {
   return (
     <>
-        <div className="card shadow mb-4">
+        <div className="card shadow mb-4 rojo">
             <div className="card-header">
                 <strong className="card-title">{name}</strong>
             </div>
-            <div className="card-body">
+            <div className="card-body" style={{
+                maxHeight: '60vh',
+                overflow: 'hidden',
+                overflowY: 'scroll',
+            }}>
                 <div className="row">
                     {children}
                 </div>
--- src/Components/PageLayout.jsx
+++ src/Components/PageLayout.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Nav } from './Nav'
-import NavUser from './NavUser'
+
 
 export const PageLayout = ({children}) => {
   return (
     <div>
         {/* <Nav/> */}
-        <NavUser/>
+        {/* <NavUser/> */}
         <div>
             {children}
         </div>
