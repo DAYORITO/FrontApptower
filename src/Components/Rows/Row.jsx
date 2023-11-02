c964631043c5eddeae64a852cc3af7fceee10dea
Emmanuel tabares <emanuel_tabares@hotmail.com>
2023-11-02 00:50:52 -0500

    Add new componets and pages

---
+++ src/Components/Rows/Row.jsx
@@ -0,0 +1,54 @@
+import "./RowsStyle.css"
+
+export const Row = ({
+    module = 'module name',
+    name = 'name',
+    lastName = 'last name',
+    docType = 'ID',
+    docNumber = '##########',
+    phone = '##########',
+    email = 'example@email.com',
+    status = 'Active',
+    children
+
+}) => {
+
+    return (
+
+        <tr class="file-list">
+
+            <td class="text-center ">
+                <div class="circle circle-sm bg-light">
+                    <span class="fe fe-user fe-16 text-muted"></span>
+                </div>
+
+                {(status == 'Active') ? <span class="dot dot-md bg-success mr-1"></span> : <span class="dot dot-md bg-danger mr-1"></span>}
+
+
+            </td>
+            <th scope="row">
+                {name + ' ' + lastName} <br />
+                <span className="badge badge-light text-muted">{docType}</span>
+                <em class="text-muted ml-2">{docNumber}</em>
+            </th>
+            <td class="text-muted">{phone}</td>
+            <td class="text-muted">{email}</td>
+            <td>
+
+                <button type="button" class="btn btn-link dropdown-toggle more-vertical p-0 text-muted mx-auto" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="text-muted sr-only">Action</span>
+                </button>
+
+                <div class="dropdown-menu m-2">
+
+                    {/* Children must be Actions Components */}
+
+                    {children}
+
+                </div>
+
+            </td>
+        </tr>
+    )
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ContainerTable } from "./Components/ContainerTable";
+import { Owners } from "./Pages/Owners/owners";
+import { Residents } from "./Pages/Residents/Residents";
 import { PageLayout } from "./Components/PageLayout";
 import VisitorsCreate from "./Pages/Visitors/VisitorsCreate";
 import LogIn from "./Pages/LogIn/LogIn";
 ReactDOM.createRoot(document.getElementById('main')).render(
-
     <React.StrictMode>
-        {/* PageLayout es la propuesta para componetizar todo el nav y el navuser pero aun no se tiene */}
-        {/* <PageLayout>
-        </PageLayout> */}
-       
-        {/* Vista de residentes, tabla */}
-        {/* <ContainerTable></ContainerTable> */}
-        {/* <VisitorsCreate /> */}
-        <LogIn />
+
+        <Residents/>
 
     </React.StrictMode>
-)
+);
