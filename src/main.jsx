a1a329ef93b7f8c83cf1b8c47c5bf30c3f7a613c
Daniel Rivera <dayorito@gmail.com>
2023-11-01 19:16:12 -0500

    Encarpetado y ejemplo de page mas scrollbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ContainerTable } from "./Components/ContainerTable";
-import Inputs from "./Components/Inputs/Inputs";
 import { PageLayout } from "./Components/PageLayout";
-import FormContainer from "./Components/FormContainer/FormContainer";
-import FormColumn from "./Components/FormContainer/FormColumn";
-
+import VisitorsCreate from "./Pages/Visitors/VisitorsCreate";
 ReactDOM.createRoot(document.getElementById('main')).render(
     
     <React.StrictMode>
+        {/* PageLayout es la propuesta para componetizar todo el nav y el navuser pero aun no se tiene */}
         {/* <PageLayout>
         </PageLayout> */}
-        <FormContainer name='Formulario'>
-            
-            <FormColumn>
-                <Inputs name="Nombre" placeholder="Ingresa tu nombre"></Inputs>
-                <Inputs name="Apellido"></Inputs>
-                <Inputs name="Edad"></Inputs>
-                <Inputs name="Fecha de nacimiento" type="Date"></Inputs>
-                <Inputs name="Email" type="Email"></Inputs>
-            
-                <Inputs name="Password" type="Password"></Inputs>
-                <Inputs name="Cabello" type="Password"></Inputs>
-                <Inputs name="Ojos" type="Password"></Inputs>
-            </FormColumn>
-            
-        </FormContainer>
+       
         {/* Vista de residentes, tabla */}
         {/* <ContainerTable></ContainerTable> */}
+        <VisitorsCreate/>
 
     </React.StrictMode>
 )
