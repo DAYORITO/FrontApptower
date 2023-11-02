3ba875441e8fe5ec35c5401af6e39841d55938b9
Alejandra2112 <alejandramejia1256@gmail.com>
2023-11-01 20:17:09 -0500

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,24 +7,24 @@ import FormContainer from "./Components/FormContainer/FormContainer";
 import FormColumn from "./Components/FormContainer/FormColumn";
 
 ReactDOM.createRoot(document.getElementById('main')).render(
-    
+
     <React.StrictMode>
         {/* <PageLayout>
         </PageLayout> */}
         <FormContainer name='Formulario'>
-            
+
             <FormColumn>
                 <Inputs name="Nombre" placeholder="Ingresa tu nombre"></Inputs>
                 <Inputs name="Apellido"></Inputs>
                 <Inputs name="Edad"></Inputs>
                 <Inputs name="Fecha de nacimiento" type="Date"></Inputs>
                 <Inputs name="Email" type="Email"></Inputs>
-            
+
                 <Inputs name="Password" type="Password"></Inputs>
                 <Inputs name="Cabello" type="Password"></Inputs>
                 <Inputs name="Ojos" type="Password"></Inputs>
             </FormColumn>
-            
+
         </FormContainer>
         {/* Vista de residentes, tabla */}
         {/* <ContainerTable></ContainerTable> */}
