10fd00fc04efb88ed02888c061535815108095a3
Alejandra2112 <alejandramejia1256@gmail.com>
2023-11-19 19:34:38 -0500

    changes in rols

--- src/Pages/Rols/RolsCreate.jsx
+++ src/Pages/Rols/RolsCreate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FormContainer from '../../Components/Forms/FormContainer';
 import FormColumn from '../../Components/Forms/FormColumn';
 import Inputs from '../../Components/Inputs/Inputs';
@@ -10,65 +10,39 @@ import { useNavigate } from 'react-router-dom';
 import './Rols.css';
 
 export const RolsCreate = () => {
-    const [testRoles, setTestRoles] = useState([]);
+    const [privileges, setPrivileges] = useState([]);
+    const [permissionsList, setPermissionsList] = useState([]);
+    const [permisos, setPermisos] = useState([]);
 
-    const permisos = [
-        {
-            label: 'Usuarios',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-        {
-            label: 'Espacios',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-
-        {
-            label: 'Vigilantes',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-
-        {
-            label: 'Residentes',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-        {
-            label: 'Propietarios',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-        {
-            label: 'Apartamentos',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-        {
-            label: 'Ingresos',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-        {
-            label: 'Visitantes',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-        {
-            label: 'Vehiculos',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-        {
-            label: 'Parqueaderos',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-        {
-            label: 'Reservas',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-        {
-            label: 'Multas',
-            options: ['Listar', 'Registrar', 'Editar'],
-        },
-        {
-            label: 'Notificaciones',
-            options: ['Listar', 'Registrar', 'Editar'],
+    useEffect(() => {
+        async function fetchPrivileges() {
+            const response = await fetch('https://apptowerbackend.onrender.com/api/privileges');
+            const data = await response.json();
+            setPrivileges(data.privileges);
         }
-    ]
 
+        async function fetchPermissions() {
+            const response = await fetch('https://apptowerbackend.onrender.com/api/permissions');
+            const data = await response.json();
+            setPermissionsList(data.permission);
+        }
+
+        fetchPrivileges();
+        fetchPermissions();
+    }, []);
+
+    useEffect(() => {
+        const permisos = permissionsList.map(permission => ({
+            label: permission.permission,
+            options: privileges.map(privilege => privilege.privilege),
+        }));
+
+        setPermisos(permisos);
+    }, [permissionsList, privileges]);
+
+
+
+    const [testRoles, setTestRoles] = useState([]);
     const navigate = useNavigate();
 
     const handlesomething = (option, permissions, hola) => {
@@ -77,8 +51,8 @@ export const RolsCreate = () => {
         console.log(hola, 'hola')
         const permissionArrive = option == 'Vigilantes' ? 'watchman' : option == 'Espacios' ? 'spaces' : 'Residentes';
         const newPermissionToAdd = permissions.map((permiso) => ({
-            permiso: option == 'Vigilantes' ? 'watchman' : option == 'Espacios' ? 'spaces' : 'Residentes',
-            privilege: permiso == 'Listar' ? 'get' : permiso == 'Registrar' ? 'post' : 'put',
+            permiso: option == 'Vigilantes' ? 'Vigilantes' : option == 'Espacios' ? 'spaces' : 'Residentes',
+            privilege: permiso == 'Listar' ? 'Listar' : permiso == 'Registrar' ? 'post' : 'put',
         }));
         console.log(newPermissionToAdd, 'newPermissionToAdd')
         const newPermisosFilter = testRoles.filter((permiso) => permiso.permiso !== permissionArrive);
@@ -87,29 +61,22 @@ export const RolsCreate = () => {
         setTestRoles(newPermissions);
         console.log(testRoles, 'testRoles')
     }
+
     const [namerole, setNamerole] = useState('');
     const [description, setDescrption] = useState('');
 
-
     const handleSubmit = async (event) => {
         event.preventDefault();
-        // const url = 'http://localhost:3000/api/rols';
         const url = 'https://apptowerbackend.onrender.com/api/rols';
-
         const data = {
             namerole,
             description,
             detailsRols: testRoles,
-
         };
-
-        console.log('Data:', data);
-        console.log('This is a test: ', testRoles)
 
         const { response, error } = await useFetchpost(url, data);
 
         if (response) {
-            console.log('Response:', response);
             Swal.fire({
                 title: 'Éxito',
                 text: 'Rol creado exitosamente',
@@ -120,7 +87,6 @@ export const RolsCreate = () => {
         }
 
         if (error) {
-            console.log('Hubo un error');
             Swal.fire({
                 title: 'Error',
                 text: 'Error al crear rol',
@@ -140,7 +106,7 @@ export const RolsCreate = () => {
                     <Inputs name='Nombre Rol' type='text' value={namerole} onChange={(e) => setNamerole(e.target.value)} />
                     <Inputs
                         name='Descripción'
-                        id='description'
+
                         value={description}
                         onChange={(e) => setDescrption(e.target.value)}
                         type='text'
@@ -149,7 +115,6 @@ export const RolsCreate = () => {
 
                 <FormColumn>
                     <div className='moduls'>
-
                         {
                             permisos.map((permiso, index) => {
                                 return (
