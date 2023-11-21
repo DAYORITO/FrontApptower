eb58c50d5eae951de773ac4c10dad51aad73a159
Alejandra2112 <alejandramejia1256@gmail.com>
2023-11-20 22:30:23 -0500

    changes in rols, and users

--- src/Components/Modals/ModalButton.jsx
+++ src/Components/Modals/ModalButton.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-function ModalButton({name, onClick}) {
+function ModalButton({ name, onClick }) {
   return (
     <>
-        {/* <!-- Button trigger modal --> */}
-        <button
-          type="button"
-          class="btn mb-2 btn-primary"
-          onClick={onClick}
-        >
-          {name}
-        </button>
+      {/* <!-- Button trigger modal --> */}
+      <button
+        type="button"
+        class="btn mb-2 btn-primary"
+        onClick={onClick}
+      >
+        {name}
+      </button>
     </>
   )
 }
